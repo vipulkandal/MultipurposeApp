cf9bb479a1063c36b34225eaf462245f1f93ec21
vipulkandal <vipulkandal@gmail.com>
2023-08-09 01:31:27 +0530

    Basic layout

--- src/components/sideBar/index.jsx
+++ src/components/sideBar/index.jsx
@@ -1,13 +1,38 @@
 import React from "react";
+import { BsFillCalculatorFill } from "react-icons/bs";
+import { TbGoGame } from "react-icons/tb";
+import { PiNotepadFill } from "react-icons/pi";
+import { TiWeatherPartlySunny } from "react-icons/ti";
 
 import "./style.scss";
 
 const SideBar = () => {
   return (
     <div className="sidebar">
-      <p className="sidebar__element">Calculator</p>
-      <p className="sidebar__element">To-Do</p>
-      <p className="sidebar__element">Weather</p>
+      <p className="sidebar__element">
+        <span>
+          <BsFillCalculatorFill />
+        </span>
+        Calculator
+      </p>
+      <p className="sidebar__element">
+        <span>
+          <PiNotepadFill />
+        </span>
+        To-Do
+      </p>
+      <p className="sidebar__element">
+        <span>
+          <TiWeatherPartlySunny />
+        </span>
+        Weather
+      </p>
+      <p className="sidebar__element">
+        <span>
+          <TbGoGame />
+        </span>
+        Tic Tac Toe
+      </p>
     </div>
   );
 };
